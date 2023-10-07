e84523f75fc3ebf1d37eea9fbde31d8834495955
pranjal <pranjalkar99@gmail.com>
2023-10-07 16:00:11 +0000

    Updated api to handle new everytime, with dynamic checking to prevent duplicacy

--- pipeline_new.py
+++ pipeline_new.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import requests
+import shutil
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
 from pymongo.mongo_client import MongoClient
@@ -42,6 +43,35 @@ def mongosearch(file_name, uid_mongo_db):
     }
     return db.find(query)
 
+def clean_pdfs_folder():
+    pdfs_folder = 'pdfs'
+    for filename in os.listdir(pdfs_folder):
+        file_path = os.path.join(pdfs_folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            logging.error('Failed to delete %s. Reason: %s', file_path, e)
+
+
+def clean_temp_output_folder():
+    output_folder = 'temp_output'
+    for item in os.listdir(output_folder):
+        item_path = os.path.join(output_folder, item)
+        try:
+            if os.path.isfile(item_path) or os.path.islink(item_path):
+                os.unlink(item_path)
+            elif os.path.isdir(item_path):
+                shutil.rmtree(item_path)
+        except Exception as e:
+            logging.error('Failed to delete %s. Reason: %s', item_path, e)
+
+
+
+
+statuses = []
 
 @pipe.get("/")
 async def root_message():
@@ -50,36 +80,94 @@ async def root_message():
 
 @pipe.get("/start-batch")
 async def start_batch(limit_pdf: int = 10):
-    os.makedirs('pdfs', exist_ok=True)
-    count = 0
-    response = requests.get(url)
+    try:
+        clean_pdfs_folder()
+        clean_temp_output_folder()
+        import os
 
-    soup = BeautifulSoup(response.content, 'html.parser')
-    tables = soup.find_all('table')
-    statuses = []
-    pdf_links = []
+        # Get the current directory
+        current_directory = os.getcwd()
 
-    for table in tables:
-        links = table.find_all('a', href=lambda href: (href and href.endswith('.pdf')))
-        for link in links:
+        # List all files in the current directory
+        files = os.listdir(current_directory)
+
+        # Iterate through the files and remove .png files
+        for file in files:
+            if file.endswith(".png") or file.endswith(".jpeg") or file.endswith(".html"):
+                file_path = os.path.join(current_directory, file)
+                os.remove(file_path)
+                print(f"Removed: {file_path}")
+    except Exception as e:
+        logging.warning('Failed to clean file,', e)
+
+    try: 
+        os.makedirs('pdfs', exist_ok=True)
+        count = 0
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        tables = soup.find_all('table')
+
+        pdf_links_file = "pdf_links.txt"
+        if os.path.exists(pdf_links_file):
+            with open(pdf_links_file, 'r') as f:
+                pdf_links = f.read().splitlines()
+        else:
+            pdf_links = []
+
+
+        for table in tables:
+            links = table.find_all('a', href=lambda href: (href and href.endswith('.pdf')))
+            for link in links:
+                if count >= limit_pdf:
+                    break
+                if link['href'] not in pdf_links:
+                    pdf_links.append(link['href'])
+                    with open(pdf_links_file, 'a') as f:
+                        f.write(link['href'] + '\n')
+                    download_link = "https://www.dir.ca.gov" + link['href']
+                    download_pdf(download_link, "pdfs/" + link.get_text() + ".pdf")
+                    count += 1
+
             if count >= limit_pdf:
                 break
-            if link['href'] not in pdf_links:
-                pdf_links.append(link['href'])
-                download_link = "https://www.dir.ca.gov" + link['href']
-                download_pdf(download_link, "pdfs/" + link.get_text() + ".pdf")
-                count += 1
 
-        if count >= limit_pdf:
-            break
+        succes = make_batch('pdfs', 'temp_output')
+        gcp_status = upload_folder_to_gcs(gcs_new_input_bucket,'temp_output/json_files')
+        if succes and gcp_status:
+            pdf_files = os.listdir("pdfs")
+            for pdf_file in pdf_files:
+                statuses.append({"pdf": pdf_file, "status": "Processed and uploaded"})
+        else:
+            statuses.append({"pdf": pdf_file, "status": "Batch Processing or uploading failed"})
+            logging.info('Successfully processed these', statuses)
+
+
+
+        return {"message": "Batch started.", "statuses": statuses}
     
-    succes = make_batch('pdfs', 'temp_output')
-    gcp_status = upload_folder_to_gcs(gcs_new_input_bucket,'temp_output/json_files')
-    if succes and gcp_status:
-        pdf_files = os.listdir("pdfs")
-        for pdf_file in pdf_files:
-            statuses.append({"pdf": pdf_file, "status": "Processed and uploaded"})
-    else:
-        statuses.append({"pdf": pdf_file, "status": "Batch Processing or uploading failed"})
+    except Exception as e:
+        logging.warning("MAJOR BATCH PROBLEM:----->",e)
+        return {"message": "Some error occured, check logs."}
 
-    return {"message": "Batch started.", "statuses": statuses}
+@pipe.get("/get-status")
+async def get_batch_status():
+    # You can return the statuses list as the current status of the batch processing/upload
+    return {"statuses": statuses}
+
+@pipe.get("/logs")
+async def get_logs():
+    with open('app.log', 'r') as log_file:
+        logs = log_file.read()
+    return {"logs": logs}
+
+
+@pipe.get("/processed")
+async def get_pdf_links():
+    pdf_links_file = "pdf_links.txt"
+    if os.path.exists(pdf_links_file):
+        with open(pdf_links_file, 'r') as f:
+            pdf_links = f.read().splitlines()
+    else:
+        pdf_links = []
+    return {"pdf_links": pdf_links}
